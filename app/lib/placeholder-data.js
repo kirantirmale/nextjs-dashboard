97771a87b81238fafd53e89143e7a1c8308a234b
Kiran Tirmale <127860308+kirantirmale@users.noreply.github.com>
2024-03-22 18:02:12 +0530

    commit

--- app/lib/placeholder-data.js
+++ app/lib/placeholder-data.js
@@ -15,7 +15,7 @@ const customers = [
     email: 'delba@oliveira.com',
     image_url: '/customers/delba-de-oliveira.png',
   },
-  
+
   {
     id: '3958dc9e-742f-4377-85e9-fec4b6a6442a',
     name: 'Lee Robinson',
