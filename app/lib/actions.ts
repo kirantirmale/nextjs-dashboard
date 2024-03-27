429b1ca14e9119ded8b723a45b9b2047ffb2994e
Kiran Tirmale <127860308+kirantirmale@users.noreply.github.com>
2024-03-27 10:34:41 +0530

    commit

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -109,6 +109,7 @@ export async function authenticate(
 ) {
   try {
     await signIn('credentials', formData);
+    console.log(formData)
   } catch (error) {
     if (error instanceof AuthError) {
       switch (error.type) {
