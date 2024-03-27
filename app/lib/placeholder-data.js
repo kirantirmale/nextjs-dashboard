d90dddf8d72e8ec88322bd519969293aab61232b
Kiran Tirmale <127860308+kirantirmale@users.noreply.github.com>
2024-03-27 10:21:23 +0530

    commit

--- app/lib/placeholder-data.js
+++ app/lib/placeholder-data.js
@@ -3,8 +3,8 @@ const users = [
   {
     id: '410544b2-4001-4271-9855-fec4b6a6442a',
     name: 'User',
-    email: 'kiran@gmail.com',
-    password: 'kiran',
+    email: 'user@nextmail.com',
+    password: '123456',
   },
 ];
 
